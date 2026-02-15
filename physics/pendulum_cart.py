aa404f5a410e507f27b713ef9ded5fa8eb9b1434
Nikita Rozanov <roznik20@Nikitas-MacBook-Pro-1130.local>
2026-02-15 10:38:51 -0500

    finally

--- physics/pendulum_cart.py
+++ physics/pendulum_cart.py
@@ -6,65 +6,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
-m1 = 1
-m2 = 1
-g = 10
-r = 1
 
-def fun(t, y):
-    th, w, x, v = y
-    theta_tt = (((m2*(w**2)*np.sin(th)*np.cos(th)) - ((g/r)*(m1+m2)*np.sin(th)))
-                /(m1+m2-(m2*np.cos(th)**2)))
-    x_tt = (-(m2*r/(m1+m2)) * ((theta_tt*np.cos(th)) - (w**2)*np.sin(th)))
-    return [w, theta_tt, v, x_tt]
 
-y0 = [0, -4, 1, 2]
-t_eval = np.linspace(0, 10, 1000)
-sol = scipy.integrate.solve_ivp(fun, (0, 10), y0, t_eval=t_eval)
 
-# Extract solution
-theta = sol.y[0]
-x_cart = sol.y[2]
+def simulate_pendulum(m1 =1 , m2 = 1, g=9.81, r=1, theta = 1.0, omega = 0.0, x = 0.0, v = 0.0, t_max=600, fps=60):
 
-# Pendulum position
-x_pend = x_cart + r * np.sin(theta)
-y_pend = -r * np.cos(theta)
+    def f(t, y):
+        th, w, x, v = y
+        theta_tt = (((m2*(w**2)*np.sin(th)*np.cos(th)) - ((g/r)*(m1+m2)*np.sin(th)))
+                    /(m1+m2-(m2*np.cos(th)**2)))
+        x_tt = (-(m2*r/(m1+m2)) * ((theta_tt*np.cos(th)) - (w**2)*np.sin(th)))
+        return [w, theta_tt, v, x_tt]
+
+    y0 = [theta, omega, x, v]
+    t_eval = np.linspace(0, t_max, int(t_max * fps))
+    sol = scipy.integrate.solve_ivp(f, (0, t_max), y0, t_eval=t_eval)
+
+    # Extract solution
+    theta = sol.y[0]
+    x_cart = sol.y[2]
+    y_cart = np.zeros_like(x_cart)  # Cart moves along x-axis, y is constant
+
+    # Pendulum position
+    x_pend = x_cart + r * np.sin(theta) 
+    y_pend = -r * np.cos(theta)
+    return t_eval, x_pend, y_pend, x_cart, y_cart
+
 
 # --- Animation ---
+t, x_pend, y_pend, x_cart, y_cart = simulate_pendulum()
 
 fig, ax = plt.subplots()
-ax.set_xlim(-5, 5)
-ax.set_ylim(-2, 2)
-ax.set_aspect('equal')
+ax.set_xlim(-1,1)
+ax.set_ylim(-1, 1)
+ax.set_aspect("equal", adjustable="box")
+line, = ax.plot([], [], "o-", lw=2)
 
-# Cart rectangle
-cart_width = 0.6
-cart_height = 0.3
-cart = plt.Rectangle((0, 0), cart_width, cart_height, fc='black')
-ax.add_patch(cart)
+def init():
+    line.set_data([], [])
+    return (line,)
 
-# Pendulum rod
-line, = ax.plot([], [], 'o-', lw=2)
+def update(i):
+    line.set_data([x_cart[i], x_pend[i]], [y_cart[i], y_pend[i]])
+    return (line,)
 
-
-def update(frame):
-    # Update cart position
-    cart.set_xy((x_cart[frame] - cart_width / 2, -cart_height / 2))
-
-    # Update pendulum rod
-    thisx = [x_cart[frame], x_pend[frame]]
-    thisy = [0, y_pend[frame]]
-    line.set_data(thisx, thisy)
-
-    return cart, line
-
-
-ani = FuncAnimation(
-    fig,
-    update,
-    frames=len(t_eval),
-    interval=15,
-    blit=False
-)
-
+ani = FuncAnimation(fig, update, frames=len(t), init_func=init, blit=True, interval=1000 / 60)
 plt.show()
